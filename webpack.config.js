9787c45b92666ea364b04528eee2749fe974a703
Stepan <stepn1k@yandex.by>
2021-11-03 22:21:55 +0300

    heroku deploy

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,28 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.tsx',
   output: {
-    path: path.join(__dirname, '/dist'),
-    filename: 'bundle.js',
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.[contenthash].js',
   },
   resolve: {
     extensions: ['.tsx', '.ts', '.js'],
   },
   module: {
     rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.s[ac]ss$/i,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
-      },
-    ],
+      { test: /\.tsx?$/, use: 'ts-loader', exclude: /node_modules/ },
+      { test: /\.scss$/, use: [{ loader: MiniCssExtractPlugin.loader }, 'css-loader', 'sass-loader'] },
+    ]
   },
   plugins: [
-    new HtmlWebpackPlugin({
-      template: './www/index.html',
-    }),
+    new HtmlWebpackPlugin({ template: './src/index.html', }),
+    new CopyPlugin({ patterns: [{ from: 'src/assets', to: 'assets' }] }),
+    new MiniCssExtractPlugin({ filename: 'style-[contenthash].css' }),
   ],
   devServer: {
     static: {
